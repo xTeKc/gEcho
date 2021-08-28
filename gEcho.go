6edcef0284b9268a1372557fa9c2dbf3e97d7954
xTeKc <bcd3v@pm.me>
2021-08-27 22:17:26 -0400

    finish echo func

--- gEcho.go
+++ gEcho.go
@@ -8,4 +8,26 @@ import (
 //echo func echoes received data
 func echo(conn net.Conn) {
 	defer conn.Close()
+
+	//buffer to store received data
+	b := make([]byte, 512) 
+	for {
+		//receive data via conn.Read into a buffer
+		size, err := conn.Read(b[0:])
+		if err == io.EOF {
+			log.Println("Client Disconnected")
+			break
+		}
+		if err != nil {
+			log.Println("Unexpected Error")
+			break
+		}
+		log.Printf("Received %d bytes: %s\n", size, string(b))
+
+		//send data via conn.Write
+		log.Println("Writing Data")
+		if _, err := conn.Write(b[0:size]); err != nil {
+			log.Fatalln("Unable to Write Data")
+		}
+	}
 }
